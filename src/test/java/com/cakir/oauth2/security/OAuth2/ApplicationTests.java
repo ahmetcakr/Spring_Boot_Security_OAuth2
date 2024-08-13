2303f83d228e1610b24140e3ed35fc36ef042053
Ahmet ÇAKIR <ahmetcaakirr@gmail.com>
2024-08-13 23:40:42 +0300

    Project init

---
+++ src/test/java/com/cakir/oauth2/security/OAuth2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cakir.oauth2.security.OAuth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
